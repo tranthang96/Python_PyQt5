9f775e9a041ecf431c99d19d29fae1eae9edd868
Tran Thang <tranvietthang196@gmail.com>
2019-08-27 14:38:27 +0700

    document

---
+++ hello.py
@@ -0,0 +1,14 @@
+from PyQt5.QtWidgets import *
+import sys
+class Window(QWidget):
+	def __init__(self):
+		QWidget.__init__(self)
+		self.setWindowTitle("Hello")
+		layout = QGridLayout()
+		self.setLayout(layout)
+		label = QLabel("Hello, World!, Minh la Thang")
+		layout.addWidget(label, 0, 0)
+app = QApplication(sys.argv)
+screen = Window()
+screen.show()
+sys.exit(app.exec_())
